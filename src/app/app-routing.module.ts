fea36788d3fc0f636c82834b8713f876cfac8bdd
valviveros <62762534+valviveros@users.noreply.github.com>
2022-01-26 07:52:31 -0500

    Added to the project ngx-admin template, made some configurations and layed out the login page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/public/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/private/home/home.module').then(
+      (m) => m.HomeModule 
+    )
+  },
+  { path: '**', redirectTo: '/login'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
