cc34197a1c5d231bcfa7ef2eebb2c24d04416c8f
Diego Perez <yeyo2k3@gmail.com>
2022-04-06 11:47:55 -0500

    CLARISA enable Insitution services and module frontend

--- src/app/pages/private/home/sections/institutions/institutions.component.ts
+++ src/app/pages/private/home/sections/institutions/institutions.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalDataSource } from 'ng2-smart-table';
+import { NbDialogService } from '@nebular/theme';
+import { InstitutionService } from 'src/app/shared/services/institution.service';
 
 @Component({
   selector: 'app-institutions',
@@ -6,10 +9,73 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./institutions.component.scss']
 })
 export class InstitutionsComponent implements OnInit {
+  settings = {
+    mode: 'external',
+    add: {
+      addButtonContent: '<i class="fas fa-plus"></i>',
+      createButtonContent: '<i class="fas fa-check"></i>',
+      cancelButtonContent: '<i class="fas fa-times"></i>',
+    },
+    actions: {
+      edit: false,
+      delete: false,
+      custom: [
+        {
+          name: 'edit',
+          title: '<i class="fas fa-pencil-alt"></i>'
+        },       
+        {
+          name: 'delete',
+          title: '<i class="fas fa-trash-alt"></i>'
+        },        
+      ],
+    },
+    columns: {
+       id: {
+         title: 'ID',
+         type: 'number',
+         width: '5%',
+       },
+       name: {
+         title: 'Name',
+         type: 'string',
+       },
+       acronym: {
+         title: 'Acronym',
+         type: 'string',
+       },
+       websiteLink: {
+         title: 'Website',
+         type: 'string',
+       },
+       added: {
+         title: 'Added Date',
+         type: 'string',
+       },
+       institutionType: {
+         title: 'Institution Type',
+         type: 'string',
+         valuePrepareFunction: (value: any) => {
+          return value.name;
+        }         
+       },
+     }
+  };
+  source: LocalDataSource = new LocalDataSource();
+  data: any;
 
-  constructor() { }
+  constructor(private dialogService: NbDialogService, private institutionService: InstitutionService) { }
 
   ngOnInit(): void {
+    this.institutionService.getInstitution().subscribe(x => {
+      this.source.load(x);
+    });
   }
 
+  addInstitution() {
+  }
+
+  onCustom(event: any) {
+    let action = event['action'];
+  }
 }
