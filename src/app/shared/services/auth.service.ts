7a4c7fe62334e1556eec3f2b92a75e356f68786a
German Martinez <g.martinez@cgiar.org>
2022-04-20 17:25:30 -0500

    Logging out an user automatically after the token has expired

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { BehaviorSubject, Observable, of, Subscription } from 'rxjs';
+import { map, delay } from 'rxjs/operators';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { User } from '../interfaces/User';
 import * as moment from 'moment';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +13,9 @@ import * as moment from 'moment';
 export class AuthService {
   private currentUserSubject!: BehaviorSubject<User>;
   public currentUser!: Observable<User>;
+  private expirationSubscription: Subscription = new Subscription();
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private router: Router) {
     let user = localStorage.getItem('currentUser');
     if (user) {
       this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(user));
@@ -28,13 +30,13 @@ export class AuthService {
     return null as any;
   }
 
-  private get isUserOnStorage() : boolean {
-    let user : string = localStorage.getItem('currentUser')??'';
+  private get isUserOnStorage(): boolean {
+    let user: string = localStorage.getItem('currentUser') ?? '';
     return user.trim().length > 0;
   }
 
   public get isLoggedIn(): boolean {
-    const expiration : moment.Moment = moment(Number(this.currentUserValue.expiresIn));
+    const expiration: moment.Moment = moment(Number(this.currentUserValue.expiresIn));
     if (this.currentUserValue && moment().isBefore(expiration)) {
       return true;
     } else {
@@ -62,12 +64,24 @@ export class AuthService {
       if (this.currentUserSubject) {
         this.currentUserSubject.next(user);
       }
+      this.setExpirationSubscription(user.expiresIn);
       return user;
     }));
   }
 
+  private setExpirationSubscription(expiresIn: number): void {
+    this.expirationSubscription.unsubscribe();
+    this.expirationSubscription = of(null).pipe(delay(expiresIn)).subscribe((expired) => {
+      console.log('EXPIRED!!');
+
+      this.logout();
+      this.router.navigate(["/login"], { state: { tokenExpired: true } });
+    });
+  }
+
   logout() {
     // remove user from local storage and set current user to null
+    this.expirationSubscription.unsubscribe();
     localStorage.removeItem('currentUser');
     this.currentUserSubject.next(null as any);
   }
