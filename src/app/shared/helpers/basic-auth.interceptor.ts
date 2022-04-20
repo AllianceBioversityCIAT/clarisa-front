eb3d72c6e785cccb8489d97eabe8b4daa89bdcec
German Martinez <g.martinez@cgiar.org>
2022-04-20 16:05:49 -0500

    Fixing AuthGuard

--- src/app/shared/helpers/basic-auth.interceptor.ts
+++ src/app/shared/helpers/basic-auth.interceptor.ts
@@ -9,15 +9,19 @@ export class BasicAuthInterceptor implements HttpInterceptor {
     constructor(private authenticationService: AuthService) { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        let token = this.authenticationService.currentUserValue?.token??'';
+        let token = this.authenticationService.currentUserValue?.token;
 
-        request = request.clone({
-            setHeaders: {
-                'Cache-Control': 'no-cache',
-                Authorization: token
-            }
-        });
+        if(token){
+            const signed = request.clone({
+                setHeaders: {
+                    'Cache-Control': 'no-cache',
+                    Authorization: token
+                }
+            });
 
-        return next.handle(request);
+            return next.handle(signed);
+        } else {
+            return next.handle(request);
+        }
     }
 }
