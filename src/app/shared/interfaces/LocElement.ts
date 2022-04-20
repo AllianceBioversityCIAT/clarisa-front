f5be9de6a17b6978235a68b1f48f30f7fdf24c5d
Diego Perez <yeyo2k3@gmail.com>
2022-04-20 08:39:40 -0500

    Fix merge conflicts

--- src/app/shared/interfaces/LocElement.ts
+++ src/app/shared/interfaces/LocElement.ts
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-export interface LocElement{
+import { SimpleDTO } from "./SimpleDTO";
+export interface LocElement extends SimpleDTO{
     id: number;
     name: string;
     isoAlpha2: string;
-=======
-import { SimpleDTO } from "./SimpleDTO";
-
-export interface LocElement extends SimpleDTO{
-    isoalpha2: string;
->>>>>>> staging
 }
