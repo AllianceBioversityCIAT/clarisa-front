fe8d61efa7ecc27db3265c66d7e72974a80b81c8
German Martinez <g.martinez@cgiar.org>
2022-04-29 16:06:53 -0500

    Event handler to check if the user is logged-in/token is valid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NavigationStart, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { AuthService } from './shared/services/auth.service';
+import { EventBusService } from './shared/services/even-bus.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
   title = 'clarisa-frontend';
   show = false;
-  urls: String[]= ["/home",
-  "/home/users",
-  "/home/roles",
-  "/home/permissions",
-  "/home/institutions"
-];
+  eventBusSub?: Subscription;
+  urls: String[]= [
+    "/home",
+    "/home/users",
+    "/home/roles",
+    "/home/permissions",
+    "/home/institutions"
+] ;
 
-  constructor(private router: Router, private authenticationService: AuthService) { 
+  constructor(
+    private router: Router, 
+    private authenticationService: AuthService,
+    private eventBusService: EventBusService
+    ) { 
     this.router.events.forEach((event) => {
       if(event instanceof NavigationStart) {
         if ((this.urls.includes(event.url)) && this.authenticationService.isLoggedIn) {
@@ -28,4 +36,15 @@ export class AppComponent {
       }
     });
   }
+
+  ngOnDestroy(): void {
+
+  }
+
+  ngOnInit(): void {
+    this.eventBusSub = this.eventBusService.on('logout', () => {
+      this.authenticationService.logout();
+    });
+  }
+
 }
