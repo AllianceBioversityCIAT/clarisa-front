97dbf0b5b36691f4df30f2750d3a6176f100e097
German Martinez <g.martinez@cgiar.org>
2022-04-29 16:09:37 -0500

    Adding refresh token handling and new field to store it

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -6,6 +6,8 @@ import { environment } from '../../../environments/environment';
 import { User } from '../interfaces/User';
 import * as moment from 'moment';
 import { Router } from '@angular/router';
+import { ACCESS_TOKEN_STORAGE_KEY, HTTP_OPTIONS_JSON, REFRESH_TOKEN_STORAGE_KEY, USER_STORAGE_KEY } from '../app-constants';
+import { RefreshToken } from '../interfaces/RefreshToken';
 
 @Injectable({
   providedIn: 'root'
@@ -13,10 +15,12 @@ import { Router } from '@angular/router';
 export class AuthService {
   private currentUserSubject!: BehaviorSubject<User>;
   public currentUser!: Observable<User>;
-  private expirationSubscription: Subscription = new Subscription();
+  //private expirationSubscription: Subscription = new Subscription();
+
+  private baseServiceUrl = `${environment.apiUrl}auth`;
 
   constructor(private http: HttpClient, private router: Router) {
-    let user = localStorage.getItem('currentUser');
+    let user = localStorage.getItem(USER_STORAGE_KEY);
     if (user) {
       this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(user));
       this.currentUser = this.currentUserSubject.asObservable();
@@ -31,58 +35,95 @@ export class AuthService {
   }
 
   private get isUserOnStorage(): boolean {
-    let user: string = localStorage.getItem('currentUser') ?? '';
+    let user: string = localStorage.getItem(USER_STORAGE_KEY) ?? '';
     return user.trim().length > 0;
   }
-
-  public get isLoggedIn(): boolean {
+  
+  public get tokenExpired() : boolean {
     const expiration: moment.Moment = moment(Number(this.currentUserValue.expiresIn));
-    if (this.currentUserValue && moment().isBefore(expiration)) {
+    if (moment().isBefore(expiration)) {
       return true;
     } else {
       return false;
     }
   }
 
+  public refreshToken(token: string) : Observable<RefreshToken> {
+    return this.http.post<RefreshToken>(`${this.baseServiceUrl}/refreshToken`, {
+      refreshToken: token
+    }, HTTP_OPTIONS_JSON);
+  }
+
+  public saveToken(accessToken : string) : void {
+    localStorage.removeItem(ACCESS_TOKEN_STORAGE_KEY);
+    localStorage.setItem(ACCESS_TOKEN_STORAGE_KEY, accessToken);
+    
+    if(this.currentUserSubject){
+      let updatedUser : User = this.currentUserSubject.getValue();
+      updatedUser.token = accessToken;
+      this.currentUserSubject.next(updatedUser);
+      this.saveUser(updatedUser);
+    }
+  }
+
+  public saveUser(user : User) : void{
+    localStorage.removeItem(USER_STORAGE_KEY);
+    localStorage.setItem(USER_STORAGE_KEY, JSON.stringify(user));
+  }
+
+  public saveRefreshToken(refreshToken : string) : void{
+    localStorage.removeItem(REFRESH_TOKEN_STORAGE_KEY);
+    localStorage.setItem(REFRESH_TOKEN_STORAGE_KEY, refreshToken);
+  }
+
+  public get isLoggedIn(): boolean {
+    if(!this.currentUserValue){
+      return false;
+    }
+    
+    return this.tokenExpired;
+  }
+
   getUserAD(userEmail: string, userPassword: string) {
-    var httpHeaders = new HttpHeaders();
-    httpHeaders.append('Content-Type', 'application/json');
-    const httpOptions = {
-      headers: httpHeaders
-    };
     let params = {
       "email": userEmail,
       "password": userPassword
     }
-    return this.http.post<any>(`${environment['apiUrl']}auth/login`, params, httpOptions);
+
+    return this.http.post<any>(`${this.baseServiceUrl}/login`, params, HTTP_OPTIONS_JSON);
   }
 
   loginAD(email: string, password: string) {
     return this.getUserAD(email, password).pipe(map((user: User) => {
-      delete user.password;
-      localStorage.setItem('currentUser', JSON.stringify(user));
+      this.saveUser(user);
+      this.saveToken(user.token);
+      this.saveRefreshToken(user.refreshToken);
       if (this.currentUserSubject) {
         this.currentUserSubject.next(user);
       }
-      this.setExpirationSubscription(user.expiresIn);
+
+      //this.setExpirationSubscription(user.expiresIn);
       return user;
     }));
   }
 
-  private setExpirationSubscription(expiresIn: number): void {
+  /*private setExpirationSubscription(expiresIn: number): void {
     this.expirationSubscription.unsubscribe();
+    console.log(`expiration subscription: expires in: " ${moment(expiresIn)}`);
     this.expirationSubscription = of(null).pipe(delay(expiresIn)).subscribe((expired) => {
       console.log('EXPIRED!!');
 
       this.logout();
       this.router.navigate(["/login"], { state: { tokenExpired: true } });
     });
-  }
+  }*/
 
   logout() {
     // remove user from local storage and set current user to null
-    this.expirationSubscription.unsubscribe();
-    localStorage.removeItem('currentUser');
+    //this.expirationSubscription.unsubscribe();
+    localStorage.removeItem(USER_STORAGE_KEY);
+    localStorage.removeItem(ACCESS_TOKEN_STORAGE_KEY);
+    localStorage.removeItem(REFRESH_TOKEN_STORAGE_KEY);
     this.currentUserSubject.next(null as any);
   }
 }
