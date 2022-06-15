fb82115adb83f43844110b8934712adf52c8007c
jdmunozg <juan.david.m.g1004@gmail.com>
2022-06-15 09:27:04 -0500

    Separate into tabs, the activation created (#22)

--- src/app/pages/private/home/sections/roles/roles.component.ts
+++ src/app/pages/private/home/sections/roles/roles.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { NbDialogService } from '@nebular/theme';
+import { NbDialogRef, NbDialogService } from '@nebular/theme';
 import { LocalDataSource } from 'ng2-smart-table';
+import { ConfirmationDialogComponent } from 'src/app/shared/components/confirmation-dialog/confirmation-dialog.component';
 import { DialogAddRolePromptComponent } from 'src/app/shared/components/roles/dialog-add-role-prompt/dialog-add-role-prompt.component';
 import { DialogDeleteRolePromptComponent } from 'src/app/shared/components/roles/dialog-delete-role-prompt/dialog-delete-role-prompt.component';
 import { DialogEditRolePromptComponent } from 'src/app/shared/components/roles/dialog-edit-role-prompt/dialog-edit-role-prompt.component';
+import { Confirmation } from 'src/app/shared/interfaces/Confirmation';
 import { RoleService } from 'src/app/shared/services/role.service';
 
 @Component({
@@ -47,14 +49,63 @@ export class RolesComponent implements OnInit {
         title: 'Acronym',
         type: 'string',
       },
-      active: {
+      /*active: {
         title: 'Is Active',
         type: 'string',
         width: '10%',
         valuePrepareFunction: (value: any) => {
           return value ? 'Yes' : 'No';
         },
+      },*/
+      globalUnit: {
+        title: 'CGIAR Entity',
+        type: 'string',
+        valuePrepareFunction: (value: any) => {
+          return value.acronym;
+        },
       },
+    },
+  };
+
+  settingsDeactive = {
+    mode: 'external',
+    actions: {
+      edit: false,
+      delete: false,
+      add:false,
+      custom: [
+        {
+          name: 'edit',
+          title: '<i class="fas fa-pencil-alt"></i>'
+        },
+        {
+          name: 'active',
+          title: '<i class="fas fa-check"></i>'
+        },
+      ],
+    },
+    columns: {
+      id: {
+        title: 'ID',
+        type: 'number',
+        width: '5%'
+      },
+      description: {
+        title: 'Description',
+        type: 'string',
+      },
+      acronym: {
+        title: 'Acronym',
+        type: 'string',
+      },
+      /*active: {
+        title: 'Is Active',
+        type: 'string',
+        width: '10%',
+        valuePrepareFunction: (value: any) => {
+          return value ? 'Yes' : 'No';
+        },
+      },*/
       globalUnit: {
         title: 'CGIAR Entity',
         type: 'string',
@@ -66,13 +117,19 @@ export class RolesComponent implements OnInit {
   };
 
   source: LocalDataSource = new LocalDataSource();
+  sourceDeactive: LocalDataSource = new LocalDataSource();
   data: any;
-
+  roleUpdated: boolean = false;
+  sort = [{
+    field: "id",
+    direction: "asc"
+  }]
   constructor(private dialogService: NbDialogService, private roleService: RoleService) { }
 
   ngOnInit(): void {
     this.roleService.getRoles().subscribe(x => {
-      this.source.load(x);
+      this.source.load(x.filter((f:any)=> f.active === true));
+        this.sourceDeactive.load(x.filter((f:any)=> f.active === false));
     });
   }
 
@@ -80,15 +137,18 @@ export class RolesComponent implements OnInit {
     this.openAddRole(DialogAddRolePromptComponent, this.data);
   }
 
-  onCustom(event: any) {
+  onCustom(event: any, estado: boolean) {
     let action = event['action'];
 
     switch (action) {
       case 'edit':
-        this.editRole(event);
+        this.editRole(event, estado);
         break;
       case 'delete':
         this.deleteRole(event);
+        break;
+      case 'active':
+        this.roleActive(event); 
         break;
 
       default:
@@ -96,12 +156,15 @@ export class RolesComponent implements OnInit {
     }
   }
 
-  editRole(event: any) {
-    this.openEditRole(DialogEditRolePromptComponent, event.data);
+  editRole(event: any, estado: boolean) {
+    this.openEditRole(DialogEditRolePromptComponent, event.data, estado);
   }
 
   deleteRole(event: any) {
     this.openDeleteRole(DialogDeleteRolePromptComponent, event.data);
+  }
+  roleActive(event: any){
+    this.openActiveRole(event.data);
   }
 
   openAddRole(dialog: any, data: any) {
@@ -112,7 +175,7 @@ export class RolesComponent implements OnInit {
     }).onClose.subscribe(info => this.source.add(info));
   }
 
-  openEditRole(dialog: any, data: any) {
+  openEditRole(dialog: any, data: any, estado: boolean) {
     this.dialogService.open(dialog, {
       context: {
         id: data.id,
@@ -121,7 +184,13 @@ export class RolesComponent implements OnInit {
         active: data.active,
         globalUnit: data.globalUnit.id,
       }
-    }).onClose.subscribe(info => this.source.update(data, info));
+    }).onClose.subscribe(info => {
+      if(estado === true){
+        this.source.update(data, info);
+      }else{
+        this.sourceDeactive.update(data,info);
+      }
+    } );
   }
 
   openDeleteRole(dialog: any, data: any) {
@@ -129,7 +198,51 @@ export class RolesComponent implements OnInit {
       context: {
         data: data
       }
-    }).onClose.subscribe(info => info != undefined ? this.source.remove(data) : null);
+    }).onClose.subscribe(info => {
+      if(info != null){
+        this.source.remove(data);
+        this.sourceDeactive.add(data);
+        this.sourceDeactive.setSort(this.sort);
+        this.sourceDeactive.refresh();
+      }
+    });
   }
+
+  openActiveRole(datas:any){
+    let data: Confirmation = {body: "Are you sure you want to activate this role?", title:{messages:"Active role: ",user:`${datas.description}`}};
+    let dialogRef : NbDialogRef<ConfirmationDialogComponent> = this.dialogService.open(ConfirmationDialogComponent, {context: {data}});
+    let roleInfo = {
+      id: datas.id,
+      description: datas.description,
+      acronym: datas.acronym,
+      active: !datas.active,
+      globalUnit: {
+        id: datas.globalUnit,
+      }
+    };
+    dialogRef.onClose.subscribe(result => {
+      if(result){
+        this.roleService.updateRole
+        (roleInfo).subscribe({
+          next:(x) => {
+            this.roleUpdated = true;
+            setTimeout(() => {
+              this.roleUpdated = false;
+              //this.ref.close(x);
+            }, 3000);
+          },
+          error: (err) => {
+            console.log(err);
+          }
+        });
+        this.source.add(datas);
+        this.sourceDeactive.remove(datas);
+        this.source.setSort(this.sort);
+        this.source.refresh();
+      }
+    });
+    
+  }
+
 
 }
